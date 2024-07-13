5990a830e5d946027521ab7c248309c57658e987
kushal <kuchibhotlakushal2004@gmail.com>
2024-07-14 02:17:05 +0530

    modified:   config/allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://mytechnotes.vercel.app'
+    'https://mytechnotes.vercel.app',
+    ' https://mytechnotes.vercel.app/auth'
 ]
 
 module.exports = allowedOrigins
